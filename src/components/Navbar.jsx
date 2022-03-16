d6c6d3571e75f470147ad7e316beb4b11d0b678e
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-03-16 21:41:26 +0100

    Installed necessary styling dep and started on Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,48 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    height: 60px;
+`
+
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+`
+
+const Left = styled.div`
+    flex:1;
+`
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;   
+`
+const SearchContainer = styled.div`
+
+`
+
+const Center = styled.div`
+    flex:1;
+`
+const Right = styled.div`
+    flex:1;
+`
 
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <Wrapper>
+                <Left>
+                    <Language>EN</Language>
+                    <SearchContainer>
+                        
+                    </SearchContainer>
+                </Left>
+                <Center></Center>
+                <Right></Right>
+            </Wrapper>
+        </Container>
     );
 }
 
