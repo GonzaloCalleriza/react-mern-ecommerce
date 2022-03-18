e786f377fcc1346178d6967c8476104b4b9a82c7
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-03-18 11:37:54 +0100

    Finished styling Navbar and Announcement

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,61 @@
-import { Search } from '@material-ui/icons';
+import { Badge } from '@material-ui/core';
+import { Search, ShoppingCartOutlined } from '@material-ui/icons';
 import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
     height: 60px;
-    background-color: red
 `
 
 const Wrapper = styled.div`
     padding: 10px 20px;
     display: flex;
     justify-content: space-between;
+    align-items: center;
 `
 
 const Left = styled.div`
     flex:1;
+    display: flex;
+    align-items: center;
 `
 const Language = styled.span`
     font-size: 14px;
     cursor: pointer;   
 `
 const SearchContainer = styled.div`
+    border: 0.5px solid lightgrey;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`
 
+const Input = styled.input`
+    border: none;
 `
 
 const Center = styled.div`
     flex: 2;
+    text-align:center;
 `
+
+const Logo = styled.h1`
+    font-weight: bold;
+`
+
 const Right = styled.div`
     flex:1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
 `
+
+const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-left: 25px;
+`;
 
 const Navbar = () => {
     return (
@@ -38,11 +64,22 @@ const Navbar = () => {
                 <Left>
                     <Language>EN</Language>
                     <SearchContainer>
-                        <Search />
+                        <Search style={{color:'gray', fontSize:16}} />
+                        <Input />
                     </SearchContainer>
                 </Left>
-                <Center></Center>
-                <Right></Right>
+                <Center>
+                    <Logo >GONZO'S STORE</Logo>
+                </Center>
+                <Right>
+                    <MenuItem>REGISTER</MenuItem>
+                    <MenuItem>SIGN IN</MenuItem>
+                    <MenuItem>
+                        <Badge badgeContent={4} color='primary'>
+                            <ShoppingCartOutlined />
+                        </Badge>
+                    </MenuItem>
+                </Right>
             </Wrapper>
         </Container>
     );
