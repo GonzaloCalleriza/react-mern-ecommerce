c309f8d5eb6ba648ba42942354813021162c0474
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-03-19 20:59:45 +0100

    Structured Products, moving to styling

---
+++ src/components/Product.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div``;
+const Circle = styled.div``;
+const Image = styled.img``;
+const Info = styled.div``;
+const Icon = styled.div``;
+
+const Product = ({ item }) => {
+    return (
+        <Container>
+            <Circle />
+            <Image src={item.img} />
+            <Info>
+                <Icon>
+                    <ShoppingCartOutlined />
+                </Icon>
+                <Icon>
+                    <SearchOutlined />
+                </Icon>
+                <Icon>
+                    <FavoriteBorderOutlined />
+                </Icon>
+            </Info>
+        </Container>
+    );
+}
+
+export default Product;
