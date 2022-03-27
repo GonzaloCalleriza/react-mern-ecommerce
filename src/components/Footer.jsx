04ff3cc6b7ff00f6df78f5ee936e5554d4891e76
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-03-27 20:34:23 +0200

    Finished Footer, moving to Product List

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Facebook, Instagram, Twitter, Pinterest } from '@material-ui/icons';
+import { Facebook, Instagram, Twitter, Pinterest, Room, Phone, MailOutline } from '@material-ui/icons';
 
 const Container = styled.div``;
 
@@ -16,26 +16,60 @@ const Logo = styled.h1`
 `;
 
 const Desc = styled.p`
-    margin: 20px 0; 
+    margin: 20px 0px; 
 `;
 
 const SocialContainer = styled.div`
     display: flex;
-    align-items: center;
-    justify-content: center;
 `;
 
 const SocialIcon = styled.div`
     width: 40px;
     height: 40px;
+    border-radius: 50%;
+    color: white;
+    background-color: #${props => props.color};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
 `;
 
 const Center = styled.div`
-    flex: 1;
+  flex: 1;
+  padding: 20px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
 `;
 
 const Right = styled.div`
     flex: 1;
+    padding: 20px;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Payment = styled.img`
+    width: 50%;
 `;
 
 const Footer = () => {
@@ -47,22 +81,48 @@ const Footer = () => {
                     Offers you won't believe unless you do a click.
                 </Desc>
                 <SocialContainer>
-                    <SocialIcon>
+                    <SocialIcon  color='3B5999'>
                         <Facebook />
                     </SocialIcon>
-                    <SocialIcon>
+                    <SocialIcon color='E4405f'>
                         <Instagram />
-                    </SocialIcon>
-                    <SocialIcon>
+                    </SocialIcon >
+                    <SocialIcon color='55ACEE'>
                         <Twitter />
                     </SocialIcon>
-                    <SocialIcon>
+                    <SocialIcon color='E60023'>
                         <Pinterest />
                     </SocialIcon>
                 </SocialContainer>
             </Left>
-            <Center></Center>
-            <Right></Right>
+            <Center>
+                <Title>Useful Links</Title>
+                <List>
+                    <ListItem>Home</ListItem>
+                    <ListItem>Cart</ListItem>
+                    <ListItem>Man Fashion</ListItem>
+                    <ListItem>Woman Fashion</ListItem>
+                    <ListItem>Accessories</ListItem>
+                    <ListItem>My Account</ListItem>
+                    <ListItem>Order Tracking</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Terms</ListItem>
+                </List>
+            </Center>
+            <Right>
+                <Title>Contact</Title>
+                <ContactItem>
+                    <Room style={{marginRight:"10px"}}/> 622 Dixie Path , South Tobinchester 98336
+                </ContactItem>
+                <ContactItem>
+                    <Phone style={{marginRight:"10px"}}/> +1 234 56 78
+                </ContactItem>
+                <ContactItem>
+                    <MailOutline style={{marginRight:"10px"}} /> contact@gonzlotec.dev
+                </ContactItem>
+                <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+            </Right>
         </Container>
     );
 }
